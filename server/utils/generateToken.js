f64600bc2e40078816f77d198452d4d483c03f3f
SakshamTiwarikk <sakshamtiwari157@gmail.com>
2026-01-13 11:39:24 +0530

    Fixes in server

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,13 +1,12 @@
-import jwt from "jsonwebtoken";
-
 export const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "7d"
+    expiresIn: "7d",
   });
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict"
+    secure: true,        // MUST be true on HTTPS
+    sameSite: "none",    // MUST be 'none' for cross-site
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
